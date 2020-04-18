defe4e77aefa28d05008a34045da92268d80adfb
Anton Armishev <armishev@vigroup.ru>
2020-04-18 18:36:28 +0600

    Calculation

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -1,6 +1,11 @@
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculation {
     private static Calculation calculation = null;
@@ -97,5 +102,83 @@ public class Calculation {
 
     public float getResult() {
         return this.result;
+    }
+
+    public static void calculate(String exprecion) {
+        System.out.println(exprecion);
+
+        //=== Расскрываем скобки в выражении
+            int first_parentheses = exprecion.lastIndexOf('(');
+            while (first_parentheses > 0) {
+                int last_parentheses = first_parentheses + exprecion.substring(first_parentheses).indexOf(')') + 1;
+                String substr = exprecion.substring(first_parentheses, last_parentheses).replace("(", "").replace(")", "").replace(" ", "");
+                String expr_result = calculateExp(substr);
+                exprecion = new StringBuilder(exprecion).replace(first_parentheses, last_parentheses, expr_result.toString()).toString();
+                System.out.println(exprecion);
+
+                first_parentheses = exprecion.lastIndexOf('(');
+            }
+
+        exprecion = calculateExp(exprecion).replace("(", "").replace(")", "").replace(" ", "");
+        System.out.println(exprecion);
+    }
+
+    //=== Считаем значение выражения без скобок
+    public static String calculateExp(String exprecion) {
+        LinkedList<String> patterns = new LinkedList();
+        patterns.add("(^-)?[\\d\\.]+[\\^][\\d\\.]+");
+        patterns.add("(^-)?[\\d\\.]+[\\*][\\d\\.]+");
+        patterns.add("(^-)?[\\d\\.]+[\\/][\\d\\.]+");
+        patterns.add("(^-)?[\\d\\.]+[\\+][\\d\\.]+");
+        patterns.add("(^-)?[\\d\\.]+[-][\\d\\.]+");
+        Matcher matcher = null;
+        boolean repeat_calculate = true;
+
+        while(repeat_calculate) {
+            repeat_calculate = false;
+
+            for (String pattern : patterns) {
+                matcher = Pattern.compile(pattern).matcher(exprecion);
+                if (matcher.find()) {
+                    //=== Считаем значение полученного выражения
+                    Double matcher_res = calculateUnitExp(matcher.group(0));
+                    exprecion = Pattern.compile(pattern).matcher(exprecion).replaceFirst(matcher_res.toString());
+                    repeat_calculate = true;
+                    break;
+                }
+            }
+        }
+
+        return exprecion;
+    }
+    //=== Считаем значение элементарного выражения
+    public static Double calculateUnitExp(String exprecion) {
+        Pattern pattern_digit = Pattern.compile("(^[-]?[\\d\\.]*)|([^\\d\\.])|([\\d\\.]*$)");
+        Matcher matcher = null;
+        matcher = pattern_digit.matcher(exprecion);
+        ArrayList expr_arr = new ArrayList();
+        Double expr_result = 0.0;
+        while (matcher.find()) {
+            //System.out.println(matcher.group(0));
+            expr_arr.add(matcher.group(0));
+        }
+
+        if (expr_arr.get(1).equals("^")) {
+            expr_result = Math.pow(Double.parseDouble(expr_arr.get(0).toString()), Double.parseDouble(expr_arr.get(2).toString()));
+        }
+        if (expr_arr.get(1).equals("*")) {
+            expr_result = Double.parseDouble(expr_arr.get(0).toString()) * Double.parseDouble(expr_arr.get(2).toString());
+        }
+        if (expr_arr.get(1).equals("/")) {
+            expr_result = Double.parseDouble(expr_arr.get(0).toString()) / Double.parseDouble(expr_arr.get(2).toString());
+        }
+        if (expr_arr.get(1).equals("+")) {
+            expr_result = Double.parseDouble(expr_arr.get(0).toString()) + Double.parseDouble(expr_arr.get(2).toString());
+        }
+        if (expr_arr.get(1).equals("-")) {
+            expr_result = Double.parseDouble(expr_arr.get(0).toString()) - Double.parseDouble(expr_arr.get(2).toString());
+        }
+
+        return expr_result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,17 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+        /*SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new Window();
             }
-        });
+        });*/
+
+        String exprecion = "((1.2+3)*(532.4-2.4 + 4))";
+        Calculation.calculate(exprecion);
+
+        //String exprecion = "4.2*526.0";
+        //Calculation.calculateExp(exprecion);
+        //System.out.println(Calculation.calculateExp(exprecion));
     }
 }
